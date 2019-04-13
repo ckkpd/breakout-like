c7878b533154f25fbef14afe7468ddc9645fbe20
private-yusuke <townstock@ksf.biglobe.ne.jp>
2019-04-13 16:43:36 +0900

    Initial commit

---
+++ Assets/Scripts/Game/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float initialSpeed = 200f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(1, -1).normalized * initialSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Block")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    private bool moveRight;
+    private bool moveLeft;
+
+    public float baseSpeed = 200f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        moveRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        moveLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+    }
+
+    private void FixedUpdate()
+    {
+        float tmpSpeed = baseSpeed;
+        if ((moveRight && moveLeft) || (!moveRight && !moveLeft)) tmpSpeed = 0f;
+        else if (moveLeft) tmpSpeed *= -1;
+
+        rb2d.velocity = new Vector2(tmpSpeed, rb2d.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject);
+    }
+}
