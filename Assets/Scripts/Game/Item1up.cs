24203212c7a567f6c589d38af8cde20138d5eab7
private-yusuke <townstock@ksf.biglobe.ne.jp>
2019-05-10 16:17:29 +0900

    Add Item1up (#7)

---
+++ Assets/Scripts/Game/Item1up.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item1up : FallenSprite
+{
+    public override void OnEffect(Player player)
+    {
+        
+    }
+
+    public override void OnObtained(Player player)
+    {
+        GameController.instance.ballsLeft++;
+    }
+}
